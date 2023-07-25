0c3f3156fcad5fb09d08c0db59ffdd68a2c1b10f
K1ngHandy <a.king.life@pm.me>
2023-07-25 14:20:18 -0400

    slice

--- index.js
+++ index.js
@@ -11,6 +11,15 @@
 // console.log(totalTrials); // prints 6
 
 // Using pop to remove elements from an array
-const runTimes = [10.45, 10.55, 10.63, 10.58, 11.58];
-const slowTime = runTimes.pop(); // save last index in variable "slowTime"
-console.log(slowTime); // prints 11.58
+// const runTimes = [10.45, 10.55, 10.63, 10.58, 11.58];
+// const slowTime = runTimes.pop(); // save last index in variable "slowTime"
+// console.log(slowTime); // prints 11.58
+
+// Obtaining subarrays with slice
+const runTimes = [10.45, 10.56, 10.67, 10.34, 10.45, 10.78, 10.34, 10.45, 10.45, 10.34, 10.24, 10.25, 10.45, 10.34, 10.33];
+
+const firstFiveTrials = runTimes.slice(0, 5);
+console.log('first 5', firstFiveTrials); // prints [ 10.45, 10.56, 10.67, 10.34, 10.45 ]
+
+const lastFiveTrials = runTimes.slice(-5); // "-5" is the start index; no end needed
+console.log('last 5', lastFiveTrials); // prints [ 10.24, 10.25, 10.45, 10.34, 10.33 ]
